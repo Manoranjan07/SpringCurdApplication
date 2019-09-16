e29d4a296858efd7a53e7afc071664d5473cbff3
Manoranjan7 <jenamanoranjan262@gmail.com>
2019-09-16 08:00:44 +0530

    create file for cancel ticket process

---
+++ src/main/java/com/sca/entities/CancelTicket.java
@@ -0,0 +1,27 @@
+package com.sca.entities;
+
+public class CancelTicket {
+	
+	private String name;
+	private String ticketId;
+	private String date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTicketId() {
+		return ticketId;
+	}
+	public void setTicketId(String ticketId) {
+		this.ticketId = ticketId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
